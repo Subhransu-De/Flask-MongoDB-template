ea3718a0b8d21abb3020da67d5fe0ef2a0343f0b
Subhransu De <subhransu.de.official@gmail.com>
2024-08-08 08:16:11 +0530

    Update error handler

--- app/error_handler.py
+++ app/error_handler.py
@@ -1,20 +1,22 @@
+import logging
 from typing import Tuple
 
 from flask import jsonify, Response
 
+from app.exception.not_found_exception import NotFoundException
 
+
+# TODO: Log the error.
+# TODO: More descriptive error.
 def setup_error_handler(app) -> None:
+    @app.errorhandler(NotFoundException)
+    def handle_not_found(e: NotFoundException) -> Tuple[Response, int]:
+        return (
+            jsonify({"error": e.message if e.message else "Resource not found."}),
+            404,
+        )
+
     @app.errorhandler(Exception)
-    def handle_exception(e) -> Tuple[Response, int]:
-        code = 500
-        if hasattr(e, "code"):
-            code = e.code
-
-        # TODO: Log the error.
-        # TODO: Logging of different types of error is configurable.
-
-        response = {
-            "message": str(e),
-            "error": "Internal Server Error" if code == 500 else "Error",
-        }
-        return jsonify(response), code
+    def handle_exception(e: Exception) -> Tuple[Response, int]:
+        logging.error(e)
+        return jsonify({"error": "An internal server error has occurred."}), 500
