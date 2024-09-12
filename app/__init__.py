1db8c9f11ba65aa510b81f3b44c88813d86e3860
Subhransu De <subhransu.de.official@gmail.com>
2024-09-12 17:08:06 +0530

    Add mongoengine

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
-from typing import Dict
-
 from flask import Flask
-from injector import singleton, Module, Injector, Binder
+from flask_injector import FlaskInjector
+from injector import singleton, Module, Binder
+from mongoengine import connect
 
 from app.config import get_config
-from app.database.mongo_database import MongoDatabase
 from app.error_handler import setup_error_handler
 from app.logging_config import setup_logging
 from app.repositories.user_repository import UserRepository
@@ -13,17 +12,7 @@ from app.services.user_service import UserService
 
 
 class AppModule(Module):
-    __config: Dict
-
-    def __init__(self, config_input: Dict):
-        self.__config = config_input
-
-    def configure(self, binder: Binder, **kwargs):
-        binder.bind(
-            MongoDatabase,
-            to=MongoDatabase(self.__config.get("URI"), self.__config.get("DB_NAME")),
-            scope=singleton,
-        )
+    def configure(self, binder: Binder):
         binder.bind(UserService, to=UserService, scope=singleton)
         binder.bind(UserRepository, to=UserRepository, scope=singleton)
 
@@ -31,19 +20,11 @@ class AppModule(Module):
 def create_app() -> Flask:
     app: Flask = Flask(__name__)
     app.config.from_object(get_config())
-    app.config["INJECTOR"]: Injector = Injector(
-        AppModule(
-            {
-                "URI": app.config["MONGO_URI"],
-                "DB_NAME": app.config["MONGO_DB_NAME"],
-            }
-        )
-    )
+    connect(app.config.get("MONGO_DB_NAME"), host=app.config.get("MONGO_URI"))
+    register_blueprints(app, [user_blueprint])
     setup_logging(app)
     setup_error_handler(app)
-
-    register_blueprints(app, [user_blueprint])
-
+    FlaskInjector(app=app, modules=[AppModule()])
     return app
 
 
